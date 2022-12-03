de0a983f9c3497959cb996fff4f687076fe44598
Zoltan Kiss (KiZo) <kiss.zoltan.pti@gmail.com>
2022-12-03 12:07:50 +0100

    28. Writing Enemy Movement Code

--- Assets/Game/Scripts/Character.cs
+++ Assets/Game/Scripts/Character.cs
@@ -12,11 +12,26 @@ public class Character : MonoBehaviour
     public float gravity = -9.8f;
     private Animator _animator;
 
+    //Enemy
+    public bool IsPlayer = true;
+    private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+    private Transform TargetPlayer;
+
     private void Awake()
     {
         _cc = GetComponent<CharacterController>();
-        _playerInput = GetComponent<PlayerInput>();
         _animator = GetComponent<Animator>();
+
+        if (!IsPlayer)
+        {
+            _navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            TargetPlayer = GameObject.FindWithTag("Player").transform;
+            _navMeshAgent.speed = MoveSpeed;
+        }
+        else
+        {
+            _playerInput = GetComponent<PlayerInput>();
+        }
     }
 
     private void CalcPlayerMovement()
@@ -32,21 +47,39 @@ public class Character : MonoBehaviour
         if (_movementVelocity != Vector3.zero)
             transform.rotation = Quaternion.LookRotation(_movementVelocity);
 
-        Debug.Log(_cc.isGrounded);
-        _animator.SetBool("AirBorne",!_cc.isGrounded);
+        _animator.SetBool("AirBorne", !_cc.isGrounded);
+    }
+
+    private void CalcEnemyMovement()
+    {
+        if (Vector3.Distance(TargetPlayer.position, transform.position) >= _navMeshAgent.stoppingDistance)
+        {
+            _navMeshAgent.SetDestination(TargetPlayer.position);
+            _animator.SetFloat("Speed", 0.2f);
+        }
+        else
+        {
+            _navMeshAgent.SetDestination(transform.position);
+            _animator.SetFloat("Speed", 0f);
+        }
     }
 
     private void FixedUpdate()
     {
-        CalcPlayerMovement();
+        if (IsPlayer)
+        {
+            CalcPlayerMovement();
 
-        if (_cc.isGrounded == false)
-            _verticalVelocity = gravity;
+            if (_cc.isGrounded == false)
+                _verticalVelocity = gravity;
+            else
+                _verticalVelocity = gravity * 0.3f;
+
+            _movementVelocity += _verticalVelocity * Vector3.up * Time.deltaTime;
+
+            _cc.Move(_movementVelocity);
+        }
         else
-            _verticalVelocity = gravity * 0.3f;
-
-        _movementVelocity += _verticalVelocity * Vector3.up * Time.deltaTime;
-
-        _cc.Move(_movementVelocity);
+            CalcEnemyMovement();
     }
 }
